69e6b0e17007be814be1eacb94d8ab917e41c639
Asaf Bartov <asaf.bartov@gmail.com>
2015-10-20 15:52:48 -0700

    Initial check-in.  Basic WAV recording.

---
+++ Pronuncify/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Pronuncify
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textLog = new System.Windows.Forms.TextBox();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.fontButton = new System.Windows.Forms.Button();
+            this.labelWord = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textWordFile = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listWords = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.comboWasapiDevices = new System.Windows.Forms.ComboBox();
+            this.textLang = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOutputFolder = new System.Windows.Forms.Button();
+            this.textOutputFolder = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(25, 19);
+            this.progressBar1.Maximum = 5;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(169, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 0;
+            // 
+            // textLog
+            // 
+            this.textLog.Location = new System.Drawing.Point(22, 222);
+            this.textLog.Multiline = true;
+            this.textLog.Name = "textLog";
+            this.textLog.Size = new System.Drawing.Size(483, 182);
+            this.textLog.TabIndex = 2;
+            // 
+            // fontButton
+            // 
+            this.fontButton.Location = new System.Drawing.Point(25, 48);
+            this.fontButton.Name = "fontButton";
+            this.fontButton.Size = new System.Drawing.Size(75, 23);
+            this.fontButton.TabIndex = 3;
+            this.fontButton.Text = "Font";
+            this.fontButton.UseVisualStyleBackColor = true;
+            this.fontButton.Click += new System.EventHandler(this.fontButton_Click);
+            // 
+            // labelWord
+            // 
+            this.labelWord.AutoSize = true;
+            this.labelWord.Location = new System.Drawing.Point(22, 83);
+            this.labelWord.Name = "labelWord";
+            this.labelWord.Size = new System.Drawing.Size(48, 13);
+            this.labelWord.TabIndex = 4;
+            this.labelWord.Text = "the word";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textLang);
+            this.groupBox1.Controls.Add(this.comboWasapiDevices);
+            this.groupBox1.Controls.Add(this.btnNext);
+            this.groupBox1.Controls.Add(this.btnStop);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.labelWord);
+            this.groupBox1.Controls.Add(this.fontButton);
+            this.groupBox1.Location = new System.Drawing.Point(22, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(483, 204);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Recorder";
+            // 
+            // textWordFile
+            // 
+            this.textWordFile.Location = new System.Drawing.Point(533, 63);
+            this.textWordFile.Name = "textWordFile";
+            this.textWordFile.Size = new System.Drawing.Size(128, 20);
+            this.textWordFile.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.Location = new System.Drawing.Point(667, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Load words...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listWords
+            // 
+            this.listWords.FormattingEnabled = true;
+            this.listWords.Location = new System.Drawing.Point(533, 92);
+            this.listWords.Name = "listWords";
+            this.listWords.Size = new System.Drawing.Size(218, 212);
+            this.listWords.TabIndex = 9;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(391, 19);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(391, 48);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 6;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNext.Location = new System.Drawing.Point(391, 77);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 7;
+            this.btnNext.Text = "NEXT";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // comboWasapiDevices
+            // 
+            this.comboWasapiDevices.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboWasapiDevices.FormattingEnabled = true;
+            this.comboWasapiDevices.Location = new System.Drawing.Point(264, 21);
+            this.comboWasapiDevices.Name = "comboWasapiDevices";
+            this.comboWasapiDevices.Size = new System.Drawing.Size(121, 21);
+            this.comboWasapiDevices.TabIndex = 13;
+            // 
+            // textLang
+            // 
+            this.textLang.Location = new System.Drawing.Point(318, 48);
+            this.textLang.Name = "textLang";
+            this.textLang.Size = new System.Drawing.Size(32, 20);
+            this.textLang.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(261, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "language";
+            // 
+            // btnOutputFolder
+            // 
+            this.btnOutputFolder.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnOutputFolder.Location = new System.Drawing.Point(667, 31);
+            this.btnOutputFolder.Name = "btnOutputFolder";
+            this.btnOutputFolder.Size = new System.Drawing.Size(84, 23);
+            this.btnOutputFolder.TabIndex = 11;
+            this.btnOutputFolder.Text = "Save To...";
+            this.btnOutputFolder.UseVisualStyleBackColor = true;
+            this.btnOutputFolder.Click += new System.EventHandler(this.btnOutputFolder_Click);
+            // 
+            // textOutputFolder
+            // 
+            this.textOutputFolder.Location = new System.Drawing.Point(533, 31);
+            this.textOutputFolder.Name = "textOutputFolder";
+            this.textOutputFolder.Size = new System.Drawing.Size(128, 20);
+            this.textOutputFolder.TabIndex = 10;
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyDocuments;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 421);
+            this.Controls.Add(this.btnOutputFolder);
+            this.Controls.Add(this.textOutputFolder);
+            this.Controls.Add(this.listWords);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textWordFile);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textLog);
+            this.Name = "Form1";
+            this.Text = "Pronuncify";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textLog;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Button fontButton;
+        private System.Windows.Forms.Label labelWord;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textWordFile;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listWords;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.ComboBox comboWasapiDevices;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textLang;
+        private System.Windows.Forms.Button btnOutputFolder;
+        private System.Windows.Forms.TextBox textOutputFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ Pronuncify/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using System.Diagnostics;
+
+namespace Pronuncify
+{
+    public partial class Form1 : Form
+    {
+        private IWaveIn waveIn;
+        private WaveFileWriter writer;
+        String outputFilename;
+        String outputFolder;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Font font = new Font(this.labelWord.Font.FontFamily, 50);
+            this.labelWord.Font = font; // default
+            this.labelWord.Text = "[word here]";
+            this.outputFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            textOutputFolder.Text = this.outputFolder;
+            textLang.Text = "XX";
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+                LoadWasapiDevicesCombo();
+            }
+        }
+
+        private void LoadWasapiDevicesCombo()
+        {
+            var deviceEnum = new MMDeviceEnumerator();
+            var devices = deviceEnum.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active).ToList();
+
+            comboWasapiDevices.DataSource = devices;
+            comboWasapiDevices.DisplayMember = "FriendlyName";
+            MessageBox.Show("Can't find any input devices!  Recording won't work...");
+            btnStart.Enabled = false;
+            btnNext.Enabled = false;
+        }
+
+        private void record()
+        {
+            // start recording
+            String word = labelWord.Text;
+            Cleanup(); // WaveIn is still unreliable in some circumstances to being reused
+
+            if (waveIn == null)
+            {
+                waveIn = CreateWaveInDevice();
+            }
+            // Forcibly turn on the microphone (some programs (Skype) turn it off).
+            var device = (MMDevice)comboWasapiDevices.SelectedItem;
+            device.AudioEndpointVolume.Mute = false;
+
+            outputFilename = textLang.Text + "-" + word + ".wav";
+            writer = new WaveFileWriter(Path.Combine(outputFolder, outputFilename), waveIn.WaveFormat);
+            textLog.AppendText("Recording...");
+            waveIn.StartRecording();
+            SetControlStates(true);
+
+        }
+        private void fontButton_Click(object sender, EventArgs e)
+        {
+            // Show the dialog.
+            DialogResult result = fontDialog1.ShowDialog();
+            // See if OK was pressed.
+            if (result == DialogResult.OK)
+            {
+                // Get Font.
+                Font font = fontDialog1.Font;
+                // Set TextBox properties.
+                //this.textWord.Text = string.Format("Font is­: {0}", font.Name);
+                this.labelWord.Font = font;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                String file = openFileDialog1.FileName;
+                textWordFile.Text = file;
+                try
+                {
+                    string[] lines = File.ReadAllLines(file);
+                    foreach (string line in lines)
+                    {
+                        listWords.Items.Add(line);
+                    }
+                    
+                    Console.WriteLine(lines.Count());
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            SetControlStates(true);
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            SetControlStates(false);
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (listWords.Items.Count > 0)
+            {
+                String word = listWords.Items[0].ToString();
+                listWords.Items.RemoveAt(0);
+                labelWord.Text = word;
+                record();
+            }
+            else
+            {
+                MessageBox.Show("No more words.  Load some more on the right.");
+            }
+        }
+        void OnRecordingPanelDisposed(object sender, EventArgs e)
+        {
+            Cleanup();
+        }
+
+        private void OnButtonStartRecordingClick(object sender, EventArgs e)
+        {
+            record();
+        }
+
+        private IWaveIn CreateWaveInDevice()
+        {
+            IWaveIn newWaveIn;
+            newWaveIn = new WaveIn();
+            newWaveIn.WaveFormat = new WaveFormat(8000, 1);
+            newWaveIn.DataAvailable += OnDataAvailable;
+            newWaveIn.RecordingStopped += OnRecordingStopped;
+            return newWaveIn;
+        }
+
+        void OnRecordingStopped(object sender, StoppedEventArgs e)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new EventHandler<StoppedEventArgs>(OnRecordingStopped), sender, e);
+            }
+            else
+            {
+                FinalizeWaveFile();
+                textLog.AppendText("done.  Saved to: " + this.outputFilename+"\n");
+                progressBar1.Value = 0;
+                if (e.Exception != null)
+                {
+                    MessageBox.Show(String.Format("A problem was encountered during recording {0}",
+                                                  e.Exception.Message));
+                }
+                SetControlStates(false);
+            }
+        }
+
+        private void Cleanup()
+        {
+            if (waveIn != null)
+            {
+                waveIn.Dispose();
+                waveIn = null;
+            }
+            FinalizeWaveFile();
+        }
+
+        private void FinalizeWaveFile()
+        {
+            if (writer != null)
+            {
+                writer.Dispose();
+                writer = null;
+            }
+        }
+
+        void OnDataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (InvokeRequired)
+            {
+                //Debug.WriteLine("Data Available");
+                BeginInvoke(new EventHandler<WaveInEventArgs>(OnDataAvailable), sender, e);
+            }
+            else
+            {
+                //Debug.WriteLine("Flushing Data Available");
+                writer.Write(e.Buffer, 0, e.BytesRecorded);
+                int secondsRecorded = (int)(writer.Length / writer.WaveFormat.AverageBytesPerSecond);
+                if (secondsRecorded >= 5)
+                {
+                    StopRecording();
+                }
+                else
+                {
+                    progressBar1.Value = secondsRecorded;
+                }
+            }
+        }
+
+        void StopRecording()
+        {
+            Debug.WriteLine("StopRecording");
+            if (waveIn != null) waveIn.StopRecording();
+        }
+
+        private void OnButtonStopRecordingClick(object sender, EventArgs e)
+        {
+            StopRecording();
+        }
+
+        private void SetControlStates(bool isRecording)
+        {
+            btnStart.Enabled = !isRecording;
+            btnNext.Enabled = !isRecording;
+            btnStop.Enabled = isRecording;
+        }
+
+        private void OnOpenFolderClick(object sender, EventArgs e)
+        {
+            Process.Start(outputFolder);
+        }
+
+        private void btnOutputFolder_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            // See if OK was pressed.
+            if (result == DialogResult.OK)
+            {
+                this.outputFolder = folderBrowserDialog1.SelectedPath;
+                textOutputFolder.Text = this.outputFolder;
+            }
+        }
+
+    }
+}
